35818229caa00fe06199391fec87a9147f322d12
regerj <regerjacob@gmail.com>
2025-01-08 10:51:10 -0800

    add floating diagnostic back

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -2,15 +2,27 @@
 require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
+local map = vim.keymap.set
 
 -- EXAMPLE
 local servers = { "pyright", "bashls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
+-- Opens a floating diagnostic window with options
+local function open_float()
+    return vim.diagnostic.open_float({ border = "rounded" })
+end
+
+-- Adds room to customize the on_attach function
+local function extended_on_attach()
+    map("n", "<leader>lf", open_float, { desc = "LSP Open Floating Diagnostic Window"})
+    nvlsp.on_attach()
+end
+
 -- lsps with default config
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
+    on_attach = extended_on_attach,
     on_init = nvlsp.on_init,
     capabilities = nvlsp.capabilities,
   }
@@ -26,7 +38,7 @@ lspconfig.clangd.setup {
 }
 
 lspconfig.rust_analyzer.setup{
-  on_attach = nvlsp.on_attach,
+  on_attach = extended_on_attach,
   capabilities = nvlsp.capabilities,
   filetypes = {"rust"},
   root_dir = lspconfig.util.root_pattern("Cargo.toml"),
