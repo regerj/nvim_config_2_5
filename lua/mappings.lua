49426920e0751d388f8396bdcdc7c1aa601ec9c6
regerj <regerjacob@gmail.com>
2025-01-08 10:13:18 -0800

    add mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,12 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- These remaps allow up and down navigation inside of wrapped lines
+-- with simple j and k. Now use gj and gk to jump entire logical
+-- lines instead of visual lines
+map("n", "j", "gj")
+map("n", "k", "gk")
+map("n", "gj", "j")
+map("n", "gk", "k")
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
