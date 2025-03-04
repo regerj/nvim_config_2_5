b716f65cbf0d8b61d118ade063de14cd54982375
regerj <regerjacob@gmail.com>
2025-03-04 10:39:11 -0800

    added markdown preview

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -61,5 +61,15 @@ return {
     keys = {
       { "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open lazy git" }
     },
-  }
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && npm install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+      vim.g.mkdp_auto_start = true
+    end,
+    ft = { "markdown" },
+  },
 }
