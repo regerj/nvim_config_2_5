18ad1cec550b1a938cc99d088605151fb58d2a84
regerj <regerjacob@gmail.com>
2025-07-24 18:17:55 -0700

    got float and hover working again with borders

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -77,6 +77,15 @@ return {
     'mrcjkb/rustaceanvim',
     version = '^6', -- Recommended
     lazy = false, -- This plugin is already lazy
+    config = function ()
+      vim.g.rustaceanvim = {
+        tools = {
+          float_win_config = {
+            border = 'rounded'
+          }
+        }
+      }
+    end
   }
 --   {
 --     "mrcjkb/rustaceanvim",
