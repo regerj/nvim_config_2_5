7c6a5725f3805707f34dc8ed537da9654d5db9b5
regerj <regerjacob@gmail.com>
2025-10-06 17:18:53 +0000

    update lspconfig.lua to new pattern

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,7 +1,7 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
+local lspconfig = vim.lsp.config
 local map = vim.keymap.set
 
 -- EXAMPLE
@@ -21,21 +21,25 @@ end
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = extended_on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
+  lspconfig(lsp, { 
+    setup = {
+      on_attach = extended_on_attach,
+      on_init = nvlsp.on_init,
+      capabilities = nvlsp.capabilities,
+    }
+  })
 end
 
-lspconfig.clangd.setup {
-  on_attach = function (client, bufnr)
-    client.server_capabilities.signatureHelpProvider = false
-    nvlsp.on_attach(client, bufnr)
-  end,
+lspconfig('clangd', {
+  setup = {
+    on_attach = function (client, bufnr)
+      client.server_capabilities.signatureHelpProvider = false
+      nvlsp.on_attach(client, bufnr)
+    end,
 
-  capabilities = nvlsp.capabilities
-}
+    capabilities = nvlsp.capabilities
+  }
+})
 
 -- lspconfig.rust_analyzer.setup{
 --   on_attach = extended_on_attach,
@@ -52,20 +56,22 @@ lspconfig.clangd.setup {
 --   }
 -- }
 
-lspconfig.azure_pipelines_ls.setup {
-  cmd = { "azure-pipelines-language-server", "--stdio" },
-  root_dir = lspconfig.util.root_pattern ".pipelines/",
-  on_attach = extended_on_attach,
-  capabilities = nvlsp.capabilities,
-  filetypes = {"yaml"},
-  settings = {
-      yaml = {
-          schemas = {
-              ["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/main/service-schema.json"] = {
-                  "/.pipelines/**/*.yml",
-                  "/.pipelines/*.yml",
-              },
-          },
-      },
-  },
-}
+lspconfig('azure_pipelines_ls', {
+  setup = {
+    cmd = { "azure-pipelines-language-server", "--stdio" },
+    root_dir = ".pipelines/",
+    on_attach = extended_on_attach,
+    capabilities = nvlsp.capabilities,
+    filetypes = {"yaml"},
+    settings = {
+        yaml = {
+            schemas = {
+                ["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/main/service-schema.json"] = {
+                    "/.pipelines/**/*.yml",
+                    "/.pipelines/*.yml",
+                },
+            },
+        },
+    },
+  }
+})
